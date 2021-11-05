1e72465c44dec50b5b4e3a572e46851a24f9a8e3
SidhantR <sidhantrc02@gmail.com>
2021-11-05 16:37:49 +0530

    first commit

---
+++ constants/images.js
@@ -0,0 +1,7 @@
+const profile_photo = require("../assets/images/dummy_profile/profile_photo.png");
+// // const profile_photo = require("../assets/images/dummy_profile/paw.png");
+// const profile_photo = require("../assets/images/dummy_profile/paw.png");
+
+export default {
+    profile_photo,
+}
